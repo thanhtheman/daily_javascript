36331ea4f537fbe3e71e24070fcec8ed7567bc65
thanh quach <thanhb.quach@gmail.com>
2022-08-08 22:17:45 -0400

    failed to set up a database

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+import database from './database';
+
 let addToDoButton = document.getElementById('addToDoButton');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
+let inputWeight = document.getElementById('inputWeight');
+
 
 addToDoButton.addEventListener('click', () => {
     let paragraph = document.createElement('p');
     let weight = document.createElement('p');
+    let task = {};
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
-    weight.innerText = inputField.value;
+    weight.innerText = inputWeight.value;
+    task.inputField.value = inputField.value;
+    database.push(task);
+    weight.classList.add('paragraph-styling');
     toDoContainer.appendChild(paragraph);
-    weightContainer.appendChild(weight);
+    toDoContainer.appendChild(weight);
+    fs.appendFile('database.js', stringify(inputField.value), (err) => {
+        console.log(err)
+    })
     inputField.value = "";
+    inputWeight.value = "";
     paragraph.addEventListener('click', () => {
         paragraph.style.textDecoration ='line-through';
     });
@@ -19,6 +31,12 @@ addToDoButton.addEventListener('click', () => {
     });
 })
 
+console.log(map1);
+
+
 laserFocus.addEventListener('click', () => {
+    let paragraph = document.createElement('p');
+    let weight = document.createElement('p');
+    toDoContainer.removeChild(paragraph, weight);
 
 })
