52e5f32250e34888b85eec375f789dd098eaff24
thanh quach <thanhb.quach@gmail.com>
2022-08-08 20:51:32 -0400

    adding weight to task

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', () => {
-    let paragraph = document.createElement('p')
-    toDoContainer.appendChild(paragraph)
+    let paragraph = document.createElement('p');
+    let weight = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    weight.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    weightContainer.appendChild(weight);
+    inputField.value = "";
+    paragraph.addEventListener('click', () => {
+        paragraph.style.textDecoration ='line-through';
+    });
+    paragraph.addEventListener('dblclick', () => {
+        toDoContainer.removeChild(paragraph);
+    });
+})
+
+laserFocus.addEventListener('click', () => {
+
 })
