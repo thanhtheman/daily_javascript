067a36261a46a0e92f08db9d7cf07fb4f768555b
thanh quach <thanhb.quach@gmail.com>
2022-08-15 17:27:46 -0400

    2nd round of JS for interview

--- js/googlefrontend1 copy.js
+++ js/googlefrontend1 copy.js
@@ -1,12 +1,14 @@
 // Round 1
-const SUBMISSION_API_URL = 'http://localhost:3000/submissions';
 const QUESTION_API_BASE_URL = 'http://localhost:3000/materials';
 
 const container = document.querySelector('.wrapper');
 
 const fetchData = async () => {
-    const [response, submissionResponse] = await Promise.all([fetch(QUESTION_API_BASE_URL), fetch(SUBMISSION_API_URL)]);
-    return await Promise.all([ response.json(), submissionResponse.json()]);
+    const response = await fetch(QUESTION_API_BASE_URL);
+    console.log(response)
+    const data = await response.json();
+    console.log(data)
+    return data;
 }
 
 const getCategory= (data) => {
@@ -19,50 +21,34 @@ const getCategory= (data) => {
        }
     });
     return categoryObj;
+
 }
 
-const getSubmissionsById = (submissions) => {
-    const submissionsById = {};
-    submissions.forEach(submission => {
-        submissionsById[submission.questionId] = submission.status;
-    })
-    console.log(submissionsById)
-    return submissionsById;
-}
-
-const displayCategory = (category, data, submissionsById) => {
+const displayCategory = (category, data) => {
     const categoryDiv = document.createElement('div');
     categoryDiv.classList.add('category');
-    let correctCount = 0;
+    const h2 = document.createElement('h2')
+    h2.textContent = category;
+    categoryDiv.append(h2);
     data.forEach((element) => {
         const questionDiv = document.createElement('div')
         questionDiv.classList.add('questions');
-        const status = document.createElement('div')
-        const statusClass = submissionsById[element.id]?.toLowerCase().replace('_','-')
-        status.classList.add(statusClass ?? 'unattempted');
-        questionDiv.append(status);
-        if(submissionsById[element.id] === 'CORRECT') {
-            correctCount++;
-        }
         const h3 = document.createElement('h3');
         h3.textContent = element.name;
         questionDiv.append(h3);
         categoryDiv.append(questionDiv);
-    });
-    const h2 = document.createElement('h2')
-    h2.textContent=`${category} - ${correctCount} / ${data.length}`
-    categoryDiv.prepend(h2);
+        
+    })
     return categoryDiv;
 }
 
 const fetchAndGetData = async () => {
-    const [data, submissions] = await fetchData();
+    const data = await fetchData();
     const categoryObj = getCategory(data);
-    const submissionsById = getSubmissionsById(submissions);
     const wrapper = document.getElementById('wrapper')
     console.log(categoryObj)
     for(const [category, data] of Object.entries(categoryObj)) {
-        const categoryDiv = displayCategory(category, data, submissionsById);
+        const categoryDiv = displayCategory(category, data);
         wrapper.append(categoryDiv);
     }
 
